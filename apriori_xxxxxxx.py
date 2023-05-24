dfe1dc5d34b0056a71ef54a90186319033036dd3
francesca <melaniapedraza17@gmail.com>
2023-05-24 11:28:04 -0600

    Add Apriori Algorith

--- apriori_xxxxxxx.py
+++ apriori_xxxxxxx.py
@@ -1,99 +1,24 @@
-
 import os
 from itertools import chain, combinations
 from collections import defaultdict
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import datetime
+import pandas as pd                 # Para la manipulación y análisis de los datos
+import numpy as np                  # Para crear vectores y matrices n dimensionales
+import csv
+from apyori import apriori
+
 
 application = Flask(__name__)
 
 application.config["UPLOAD_FOLDER"] = "static/"
-
-
-def subsets(arr):
-    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
-
-
-def min_support_items(item_set, transaction_list, min_support, freq_set):
-    _item_set = set()
-    local_set = defaultdict(int)
-
-    for item in item_set:
-        for transaction in transaction_list:
-            if item.issubset(transaction):
-                freq_set[item] += 1
-                local_set[item] += 1
-
-    for item, count in local_set.items():
-        if count >= min_support:
-            _item_set.add(item)
-    return _item_set
-
-
-def combine_set(item_set, length):
-    return set(
-        [i.union(j) for i in item_set for j in item_set if len(i.union(j)) == length]
-    )
-
-
-def find_frequency_1_dataset(iterator):
-    transaction_list = list()
-    item_set = set()
-    for record in iterator:
-        transaction = frozenset(record)
-        transaction_list.append(transaction)
-        for item in transaction:
-            item_set.add(frozenset([item]))  # Generate 1-item_sets
-    return item_set, transaction_list
-
-
-def run_apriori(data_iter, min_support):
-    item_set, transaction_list = find_frequency_1_dataset(data_iter)
-    freq_set = defaultdict(int)
-    large_set = dict()
-    onec_set = min_support_items(item_set, transaction_list, min_support, freq_set)
-    currentl_set = onec_set
-    k = 2
-    while currentl_set != set([]):
-        large_set[k - 1] = currentl_set
-        currentl_set = combine_set(currentl_set, k)
-        current_set = min_support_items(
-            currentl_set, transaction_list, min_support, freq_set
-        )
-        currentl_set = current_set
-        k = k + 1
-
-    frequent_items = []
-    if not large_set:
-        return []
-    length = 1
-    while length<len(large_set):
-        for item in large_set.get(length):
-            is_superset_frequent = False
-            for superset in large_set.get(length+1):
-                if frozenset.issubset(item, superset):
-                    is_superset_frequent = True
-            if not is_superset_frequent:
-                frequent_items.append((tuple(item), freq_set[item]))
-        length = length+1
-    for item in large_set.get(length):
-        frequent_items.append((tuple(item), freq_set[item]))
-    return frequent_items
 
 def format_results(items):
     i = []
     for item, count in sorted(items, key=lambda x: x[1]):
         i.append(item)
     return set(i)
-
-
-def get_data(fname):
-    with open(fname, newline="") as file_iter:
-        for line in file_iter:
-            line = line.strip().rstrip(",")  
-            record = frozenset(list(map(str.strip, line.split(",")[1:])))
-            yield record
 
 @application.route('/')
 def upload_file():
@@ -105,37 +30,51 @@ def save_file():
     if request.method == 'POST':
         f = request.files['file']
         s = request.form['support']
-        support =int(s)
+        c =request.form['confidence']
+        l =request.form['lift']
+        #support =float(s)
+        #confidence = float(c)
+        #lift = int(l)
         filename = secure_filename(f.filename)
 
         basedir = os.path.abspath(os.path.dirname(__file__))
 
         f.save(os.path.join(basedir, application.config['UPLOAD_FOLDER'], filename))
+        filepath=os.path.join(basedir, application.config['UPLOAD_FOLDER'], filename)
         file = open(application.config['UPLOAD_FOLDER'] + filename,"r")
         content = file.read()
         
-      
-        inFile = []
-    for line in content.split('\n'):
-        line = line.strip().rstrip(",")  # Remove trailing comma
-        record = frozenset(list(map(str.strip, line.split(",")[1:]))) 
-        inFile.append(record)
-    
-    if inFile is not None:  
+       # infile = []
+
+
+    #if infile is None:  
         start = datetime.datetime.now()
-        items = run_apriori(inFile, support)
+        DatosTransacciones = pd.read_csv(filepath, header=None)
+        #Se incluyen todas las transacciones en una sola lista
+        Transacciones = DatosTransacciones.values.reshape(-1).tolist() #-1 significa 'dimensión desconocida'
+        Lista = pd.DataFrame(Transacciones)
+        Lista['Frecuencia'] = 1
+        #Se agrupa los elementos
+        Lista = Lista.groupby(by=[0], as_index=False).count().sort_values(by=['Frecuencia'], ascending=True) #Conteo
+        Lista['Porcentaje'] = (Lista['Frecuencia'] / Lista['Frecuencia'].sum()) #Porcentaje
+        Lista = Lista.rename(columns={0 : 'Item'})
+        TransaccionesLista = DatosTransacciones.stack().groupby(level=0).apply(list).tolist()
+        items = apriori(TransaccionesLista,min_support =float(s),min_confidence = float(c),min_lift = int(l))
         #print(items)
-        
         #calculating the algorithm execution time
         end = datetime.datetime.now()
         program_run_time = str((end - start))  
 
-        res= format_results(items)
-        total_item = len(items)  
+
+        #res= format_results(items)
+        ResultadosC1 = list(items)
+
+        total_item = len(ResultadosC1)
+
+      
         
 
-    return render_template('content.html', res=res ,items =items ,s=s,total_item = total_item ,filename =filename , content= content, program_run_time =program_run_time) 
-
+    return render_template('content.html', s=s,c=c, l=l, filename =filename,content=content, ResultadosC1=ResultadosC1, total_item=total_item) 
 
 
 
